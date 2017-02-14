df6a4ae26a941209fbf8a8d7bb5a82079cc3de96
Cathytvxq <liuyaxin219@126.com>
2017-02-14 12:21:11 +0800

    1 Change

--- PricePrediction/PricePrediction.R
+++ PricePrediction/PricePrediction.R
@@ -1,5 +1,5 @@
 
-# install.packages("class")
+# install.packages("forecast")
 library(class)
 library(dplyr)
 library(ggplot2)
@@ -8,6 +8,8 @@ library(lattice)
 library(caret)#train and test
 library(kernlab)#svm
 library(rpart)#tree learning
+library(grid)
+library(DMwR)#knnimputation
 
 Sys.getlocale(category = "LC_ALL")
 Sys.setlocale(category = "LC_ALL", locale = "zh_CN.UTF-8")
@@ -22,12 +24,14 @@ summary(data0)
 #对于数字型信息price/age/lat/lng/building_count/house_count,其中lat/lng只有一个缺失值，price/building_count/house_count
 #的缺失值是0，age的缺失值是2016其中的错误值是<0
 
-data1 <- filter(data0, !is.na(community_id))#去掉community_id为NA的行
-data1 <- filter(data0, (district!="") & (plate!="")) #去掉district／plate没有的行 828/21285=3.8%
-data1[(data1$age > 100),'age'] <- 0
+data0 <- filter(data0, !is.na(community_id))#data0,去掉community_id为NA的行
 
 
 ##########D3.js展示上海房价所需信息############
+
+data1 <- filter(data0, (district!="") & (plate!="")) #去掉district／plate没有的行 828/21285=3.8%
+data1[(data1$age > 100),'age'] <- 0
+
 #data1_price去掉了price为0，去掉缺失值
 data1_price <- filter(data1, (price > 0))
 
@@ -43,10 +47,10 @@ districtPrice <- data1_price %>%
   filter(minPrice > 0)
 
 
-#############分析内容1: 房价的预测##############
+#################################################分析内容1: 房价预测模型#####################################################
 ##数据预处理
 data1_price_model1 <- filter(data1_price, (age>0) & (house_count >0))
-data1_price_model1 <- select(data1_price_model1, price, age, lat, lng, house_count, building_count)
+data1_price_model1 <- select(data1_price_model1, price, age, lat, lng, building_count,house_count )
 hist(data1_price_model1$age)
 hist(data1_price_model1$house_count)
 hist(data1_price_model1$building_count)
@@ -62,14 +66,14 @@ print(fit.lm)
 # RMSE      Rsquared 
 #20540.31  0.1477599
 
-# #~~~logistic regression
-# set.seed(222)
-# train_control <- trainControl(method = "cv", number = 5)#对数据进行采样
-# fit.glm <- train(price~., data=data1_price_model1, method="glm", metric="RMSE", 
-#                  preProc=c("center", "scale"), trControl=train_control)
-# print(fit.glm)
-# #RMSE      Rsquared 
-# #20546.05  0.1474363
+#~~~logistic regression
+set.seed(222)
+train_control <- trainControl(method = "cv", number = 5)#对数据进行采样
+fit.glm <- train(price~., data=data1_price_model1, method="glm", metric="RMSE",
+                 preProc=c("center", "scale"), trControl=train_control)
+print(fit.glm)
+#RMSE      Rsquared
+#20546.05  0.1474363
 
 #~~~knn
 set.seed(333)
@@ -83,16 +87,6 @@ print(fit.knn)
 #9  12265.08  0.6968362
 #The final value used for the model was k = 9
 
-#~~~SVM 
-set.seed(444)
-train_control <- trainControl(method = "cv", number = 5)#对数据进行采样
-fit.svm <- train(price~., data=data1_price_model1, method="svmRadial", metric="RMSE", trControl=train_control)
-print(fit.svm)
-#C     RMSE      Rsquared 
-#0.25  12798.67  0.6796370
-#0.50  12504.28  0.6921360
-#1.00  12288.82  0.7011281
-#The final values used for the model were sigma = 0.3994958 and C = 1
 
 #~~~tree learning
 set.seed(555)
@@ -106,3 +100,35 @@ print(fit.tree)
 #The final value used for the model was cp = 0.08612607
 
 #knn的rmse值最小
+
+
+##############################################预测房价#####################################################
+
+sapply(data0,function(x){
+  sum(x==0,na.rm = TRUE)
+})
+# price            age            lat            lng         building_count    house_count
+# 12269             70              0              0               21              18
+
+##缺失值填补
+data1_NA <- data0
+data1_NA[data1_NA$age==2016,"age"] <-0
+data1_NA[data1_NA$age==0,"age"] <-NA
+data1_NA[data1_NA$building_count==0,"building_count"] <-NA
+data1_NA[data1_NA$house_count==0,"house_count"] <-NA
+
+data1_num_NA <- data1_NA[,c("price","age","lat", "lng","building_count","house_count" )]
+sum(is.na(data1_num_NA))
+
+data1_filling_NA<-knnImputation(data1_num_NA,k=9)
+data1_price_prediction <- filter(data1_filling_NA, price==0)
+
+
+##房价预测
+x <- as.matrix(data1_price_model1[,2:6])
+y <- as.matrix(data1_price_model1[,1])
+fitknn <- knnreg(x, y, k=9)
+predictions <- data.frame(price = predict(fitknn, newdata = data1_price_prediction[,2:6]))
+data1_price_prediction$price <- predictions
+names(data1_price_prediction)[1] <- "price"
+colnames(data1_price_prediction[,1])
